a7e6cc7ae80036e28b481e0a03eb30b93805bec6
XiaoLiu <xiaoliucool@126.com>
2016-07-01 16:11:56 +0800

    add contract and NotePresenter

---
+++ app/src/main/java/com/xiaoliu/mvp/Contract.java
@@ -0,0 +1,13 @@
+package com.xiaoliu.mvp;
+
+/**
+ * Created by Administrator on 2016/7/1.
+ */
+public class Contract {
+    interface View extends BaseView{
+
+    }
+    interface Presenter extends BasePresenter{
+
+    }
+}
---
+++ app/src/main/java/com/xiaoliu/mvp/NotePresenter.java
@@ -0,0 +1,11 @@
+package com.xiaoliu.mvp;
+
+/**
+ * Created by Administrator on 2016/7/1.
+ */
+public class NotePresenter implements Contract.Presenter {
+    @Override
+    public void start() {
+
+    }
+}
